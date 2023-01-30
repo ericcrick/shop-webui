2b9ddfc8d533cfbcafcaa4ffedb5488e39272997
ericnanaosei <eric.nana-osei@afrilogicsolutions.com>
2023-01-30 06:36:42 +0000

    deleting function

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,4 +1,31 @@
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+
 function ProductList({ products }: any){
+  const router = useRouter();
+  const handleDelete = async(event: React.FormEvent<HTMLFormElement | any>)=>{
+    event.preventDefault();
+    const decision = confirm(" Are you sure you want to delete this product")
+    if(decision !== false){
+      
+      const productId = event.currentTarget.value
+      
+      const response = await fetch(`https://shopweb-api.herokuapp.com/products/${productId}`, {
+        method: 'DELETE',
+        mode: 'cors',
+        credentials: 'same-origin',
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      const data = await response.json();
+      alert("Product deleted successfully")
+      router.reload();
+    }
+
+  }
   return (
     <>
       <td className="px-4 py-3">{products._id}</td>
@@ -7,13 +34,11 @@ function ProductList({ products }: any){
       <td className="px-4 py-3 text-lg text-gray-900">{products.productPrice}</td>
       <td className="py-4 flex justify-between">
       <div className="px-2">
-        <input name="plan" type="button"/>
         <span className="bg-green-300 hover:bg-green-600 px-3 rounded-md cursor-pointer text-white py-2">Edit</span>
       </div>
-      <div className="px-2">
-        <input name="plan" type="button"/>
+      <button value={products._id} onClick = {handleDelete} className="px-2">
         <span className="bg-red-600  hover:bg-red-300 px-3 rounded-md cursor-pointer text-white py-2">Delete</span>
-      </div>
+      </button>
       </td>
     </>
   )
--- components/user-list.tsx
+++ components/user-list.tsx
@@ -1,4 +1,30 @@
+import { useRouter } from "next/router";
+
 function UserList({ user }: any){
+  const router = useRouter();
+  const handleDelete = async(event: React.FormEvent<HTMLFormElement | any>)=>{
+    event.preventDefault();
+    const decision = confirm(" Are you sure you want to delete this user")
+    if(decision !== false){
+      
+      const userId = event.currentTarget.value
+      
+      const response = await fetch(`https://shopweb-api.herokuapp.com/user/${userId}`, {
+        method: 'DELETE',
+        mode: 'cors',
+        credentials: 'same-origin',
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      const data = await response.json();
+      alert("User deleted successfully")
+      router.reload();
+    }
+
+  }
   return (
     <>
       <td className="px-4 py-3">{user._id}</td>
@@ -6,14 +32,12 @@ function UserList({ user }: any){
       <td className="px-4 py-3">{user.email}</td>
       <td className="px-4 py-3 text-lg text-gray-900">{user.role}</td>
       <td className="py-4 flex justify-between">
-      <div className="px-2">
-        <input name="plan" type="button"/>
+      <button className="px-2">
         <span className="bg-green-300 hover:bg-green-600 px-3 rounded-md cursor-pointer text-white py-2">Edit</span>
-      </div>
-      <div className="px-2">
-        <input name="plan" type="button"/>
+      </button>
+      <button value={user._id} onClick = {handleDelete}  className="px-2">
         <span className="bg-red-600  hover:bg-red-300 px-3 rounded-md cursor-pointer text-white py-2">Delete</span>
-      </div>
+      </button>
       </td>
     </>
   )
